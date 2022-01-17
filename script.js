608b8e52170dfaf862df28b3655d952bb38176b9
Albert Jhuset Quintanilla Peralta <aj@pop-os.localdomain>
2022-01-17 18:23:39 -0500

    agregando evento click

---
+++ script.js
@@ -0,0 +1,10 @@
+let items = document.querySelectorAll(".item")
+
+items.forEach(item => {
+    item.addEventListener("click",(e) => {
+        items.forEach(item => {
+            item.classList = "item"
+        })
+        e.target.classList.toggle("active")
+    })
+})
